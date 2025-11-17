917a6a5580d2c54bc74fb06472b18402521ff7bf
Volodymyr <kotsiubkov@gmail.com>
2025-11-17 17:05:45 +0200

    start

---
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -0,0 +1,12 @@
+import css from './MovieGrid.module.css';
+
+
+
+function MovieGrid() {
+    return (
+        <p className={css.message}>No feedback yet</p>
+    )
+}
+
+
+export default MovieGrid;
